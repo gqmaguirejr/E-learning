f7999678897f0ca6707d768324f8b8806287ffa3
Gerald Q. Maguire Jr <maguire@kth.se>
2020-09-12 23:37:49 +0200

    New version of the code to process each of the buttons that invoke an external tool and new version of the document to go with this.

--- add-url-to-button-push-for-lti.js
+++ add-url-to-button-push-for-lti.js
@@ -2,6 +2,7 @@
 // @name        augement navigation button links
 // @namespace   https://github.com/gqmaguirejr/E-learning
 // @author      Gerald Q. Maguire Jr. <maguire@kth.se>
+// @description Extend the URL of buttons for external tools with URL of the page we are on.
 **/
 
 (function() {
@@ -10,11 +11,20 @@
     onPage(/\/courses\/\d+\/users/, function() {
 	hasAnyRole('teacher', function(hasRole) {
 	    if (hasRole) {
-		// do something
-		var link = document.getElementsByClassName('context_external_tool_2')[0]
-		link.setAttribute('href',
-				  link.getAttribute('href').concat('?url='+location.href))
-		
+		// process all tools
+		var links=document.querySelectorAll("[class^=context_external_tool_")
+		var i;
+		var link;
+		for (i = 0; i < links.length; i++) {
+		    link=links[i].getAttribute('href')
+		    if (link.indexOf("?") == -1) {
+			link=link.concat('?url='+location.href)
+		    } else {
+			// if there is already a question mark then there is a parameter
+			link=link.concat('&url='+location.href)
+		    }
+		    links[i].setAttribute('href',link)
+		}
 	    } else {
 		// do something else
 	    }

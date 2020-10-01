40bddd5629d05cf97edbb809aafb53e674d3c92f
Gerald Q. Maguire Jr <maguire@kth.se>
2020-10-01 15:36:00 +0200

    Now the semantics of changing the UI language also changes which tabs contents are display.

--- add_language_global_nav_item.js
+++ add_language_global_nav_item.js
@@ -5,13 +5,6 @@
 // @description Add a button labeled with the current language
 **/
 
-
-
-function getUserLocale(data) {
-    console.info("user's local is ", data['locale'])
-}
-
-
 function toggleLanguage() {
     console.info("called  toggleLanguage")
 //    '/api/v1/users/self?user[locale]=sv'
@@ -40,14 +33,46 @@ function toggleLanguage() {
 }
 
 
+// set the initial tab by adding a class "ui-tabs-active"
+// <div class="enhanceable_content tabs">
+// <ul>
+// <li lang="en"><a href="#fragment-1">English</a></li>
+// <li class="ui-tabs-active" lang="sv"><a href="#fragment-2">P&aring; svenska</a></li>
+// </ul>
+
+function modifyTabOrder() {
+    console.info("called  swapTabOrder")
+    if (ENV.LOCALE === 'en') {
+	console.info("show English first");
+	// $("div.enhanceable_content.tabs ul li[lang^='sv']").removeClass( "ui-tabs-active" )
+	// $("div.enhanceable_content.tabs ul li[lang^='en']").addClass( "ui-tabs-active" );
+	$("div.enhanceable_content.tabs ul li[lang^='en']").each(function() {
+	    $(this).click();
+	});
+    }
+    else if (ENV.LOCALE === 'sv') {
+	console.info("show Swedish first");
+	//  .ui-state-default .ui-corner-top
+	//	$("div.enhanceable_content.tabs ul li[lang^='en']").removeClass( "ui-tabs-active" )
+	//	$("div.enhanceable_content.tabs ul li[lang^='sv']").addClass( "ui-tabs-active" );
+	$("div.enhanceable_content.tabs ul li[lang^='sv']").each(function() {
+	    $(this).click();
+	});
+    }
+}
+
 (function() {
     'use strict';
 
     onPage(/\/courses/, function() {
 	//	addMenuItem("http://www.kth.se", 'icon-settings-2')
 	addMenuItem("#", 'icon-settings-2')
-
+	setTimeout(modifyTabOrder, 1000);
+	//modifyTabOrder();
     });
+
+
+
 
 
     function onPage(regex, fn) {

a3fca08c93a7164b5c3db117149136a37aab9187
Gerald Q. Maguire Jr <maguire@kth.se>
2022-04-05 23:03:50 +0200

    Reoved the ENglish KTH logo for Swedish language covers.

--- add_subject_credits_title_etc_to_cover.py
+++ add_subject_credits_title_etc_to_cover.py
@@ -198,6 +198,16 @@ def transform_file(content, dict_of_entries, exam, language, cycle, keep_picture
                 postfix=content[end_offset_1+len(end_marker_1):]
                 content=prefix + postfix
 
+    # if te language is 'sv' then change the country from "Sweden" to "Sverige"
+    sweden_xml='<w:lang w:val="en-US"/></w:rPr><w:t>Sweden</w:t>'
+    sverige_xml='<w:lang w:val="sv-SE"/></w:rPr><w:t>Sverige</w:t>'
+    content=content.replace(sweden_xml, sverige_xml)
+    
+    # if this is a Swedish language cover, then remove the English KTH logo
+    english_KTH_logo='<w:r w:rsidRPr="00A15578"><w:rPr><w:noProof/><w:lang w:val="en-US"/></w:rPr><w:drawing><wp:anchor distT="0" distB="0" distL="114300" distR="114300" simplePos="0" relativeHeight="251667456" behindDoc="1" locked="0" layoutInCell="1" allowOverlap="1" wp14:anchorId="6EB69F10" wp14:editId="7BEE6404"><wp:simplePos x="0" y="0"/><wp:positionH relativeFrom="leftMargin"><wp:posOffset>5736178</wp:posOffset></wp:positionH><wp:positionV relativeFrom="topMargin"><wp:posOffset>417830</wp:posOffset></wp:positionV><wp:extent cx="1331595" cy="240665"/><wp:effectExtent l="0" t="0" r="1905" b="6985"/><wp:wrapNone/><wp:docPr id="3" name="Bildobjekt 3" descr="English logotype for KTH Royal Institute of Technology."/><wp:cNvGraphicFramePr><a:graphicFrameLocks xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main" noChangeAspect="1"/></wp:cNvGraphicFramePr><a:graphic xmlns:a="http://schemas.openxmlformats.org/drawingml/2006/main"><a:graphicData uri="http://schemas.openxmlformats.org/drawingml/2006/picture"><pic:pic xmlns:pic="http://schemas.openxmlformats.org/drawingml/2006/picture"><pic:nvPicPr><pic:cNvPr id="3" name="Bildobjekt 3" descr="English logotype for KTH Royal Institute of Technology."/><pic:cNvPicPr/></pic:nvPicPr><pic:blipFill><a:blip r:embed="rId8" cstate="print"><a:extLst><a:ext uri="{28A0092B-C50C-407E-A947-70E740481C1C}"><a14:useLocalDpi xmlns:a14="http://schemas.microsoft.com/office/drawing/2010/main" val="0"/></a:ext></a:extLst></a:blip><a:stretch><a:fillRect/></a:stretch></pic:blipFill><pic:spPr><a:xfrm><a:off x="0" y="0"/><a:ext cx="1331595" cy="240665"/></a:xfrm><a:prstGeom prst="rect"><a:avLst/></a:prstGeom></pic:spPr></pic:pic></a:graphicData></a:graphic><wp14:sizeRelH relativeFrom="page"><wp14:pctWidth>0</wp14:pctWidth></wp14:sizeRelH><wp14:sizeRelV relativeFrom="page"><wp14:pctHeight>0</wp14:pctHeight></wp14:sizeRelV></wp:anchor></w:drawing></w:r>'
+
+    if language == 'sv':
+        content=content.replace(english_KTH_logo, '')
 
     if exam == 'kandidatexamen':
         cycle=1
@@ -1075,7 +1085,7 @@ def transform_file(content, dict_of_entries, exam, language, cycle, keep_picture
         new_subtitle_xml='w:val="Subtitle"/><w:spacing w:before="120"/></w:pPr><w:r w:rsidRPr="00A15578"><w:t>{}</w:t></w:r></w:p>'.format(subtitle)
         content=content.replace(subtitle_xml, new_subtitle_xml)
 
-        # {"Author1": {"Last name": "Student", "First name"
+    # {"Author1": {"Last name": "Student", "First name"
     author1=dict_of_entries.get('Author1', None)
     author1_first_name=author1.get('First name', None)
     author1_last_name=author1.get('Last name', None)
@@ -1177,7 +1187,7 @@ def main(argv):
 
     argp.add_argument('--language',
                       type=str,
-                      default='en',
+                      default=None,
                       help="language sv or en for Swedish or English"
                       )
 
@@ -1266,11 +1276,12 @@ def main(argv):
         title=dict_of_entries.get('Title', None)
         if title:
             language=title.get('Language', None)
-
             if language == 'eng':
                 language = 'en'
             elif language == 'swe':
                 language = 'sv'
+            else:
+                language = None
 
     if language not in ['sv', 'en']:
         print("Unknown language use 'sv' for Swedish or 'en' for English")
